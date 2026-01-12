a0f5f5e04350e9b13800cb431d519a1e2f025e09
Tom Genoni <tom@genoni.studio>
2026-01-11 16:55:03 -0800

    Remove react crud

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,16 @@
 
 
-/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+/** @type { import('@storybook/nextjs').StorybookConfig } */
 const config = {
   "stories": [
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
-    "@storybook/preset-create-react-app",
     "@storybook/addon-a11y",
     "@storybook/addon-docs"
   ],
-  "framework": "@storybook/react-webpack5",
+  "framework": "@storybook/nextjs",
   "staticDirs": [
     "../public"
   ]
